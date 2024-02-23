bd758c47dccf99b637cb5fb2f1bf8d6a2789a12b
Nick Alcorso <nalcorso@gmail.com>
2024-02-23 11:16:29 +1100

    SetsAPI: Added simple input parameter checking

--- src/ReForge.Scryfall/APIs/ScryfallSetsAPI.cs
+++ src/ReForge.Scryfall/APIs/ScryfallSetsAPI.cs
@@ -11,21 +11,31 @@ public class ScryfallSetsAPI
         _client = client;
     }
     
+    //FIXME: Add support for missing parameters: format, pretty
     public Task<Optional<ListObject<Set>>> AllAsync()
     {
         return _client.GetAsync<ListObject<Set>>("sets");
     }
     
+    //FIXME: Add support for missing parameters: format, pretty
     public Task<Optional<Set>> ByCodeAsync(string code)
     {
+        if (string.IsNullOrEmpty(code))
+            throw new ArgumentException("Value cannot be null or empty.", nameof(code));
+
+        if (code.Length < 3 || code.Length > 5)
+            throw new ArgumentException("Value must be between 3 and 5 characters long.", nameof(code));
+        
         return _client.GetAsync<Set>($"sets/{code}");
     }
     
+    //FIXME: Add support for missing parameters: format, pretty
     public Task<Optional<Set>> ByTcgPlayerIdAsync(int id)
     {
         return _client.GetAsync<Set>($"sets/tcgplayer/{id}");
     }
     
+    //FIXME: Add support for missing parameters: format, pretty
     public Task<Optional<Set>> ByIdAsync(Guid id)
     {
         return _client.GetAsync<Set>($"sets/{id}");
