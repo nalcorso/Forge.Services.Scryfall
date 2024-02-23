218a1529282040329d289d44a46a39432cb2c178
Nick Alcorso <nalcorso@gmail.com>
2024-02-23 11:17:14 +1100

    Scratch: Added API calls to new functions

--- samples/ReForge.Scryfall.BasicSyntax/Program.cs
+++ samples/ReForge.Scryfall.BasicSyntax/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text;
 using Dumpify;
 using Microsoft.Extensions.Logging;
 using ReForge.Scryfall;
+using ReForge.Scryfall.APIs;
+using ReForge.Scryfall.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
 
 Console.WriteLine("Scryfall Scratchpad - Basic Syntax");
 
@@ -17,14 +23,28 @@ var scryfallClient = ScryfallClient.Create()
     .WithDefaultCache()
     .Build();
 
-//var randomCard = await scryfallClient.Cards.RandomAsync();
-//randomCard.Dump();
 
-//var setTest = await scryfallClient.Sets.AllAsync();
-//var setTest = await scryfallClient.Sets.ByCodeAsync("uma");
-//var setTest = await scryfallClient.Sets.ByTcgPlayerIdAsync(1909);
-//var setTest = await scryfallClient.Sets.ByIdAsync(Guid.Parse("2ec77b94-6d47-4891-a480-5d0b4e5c9372"));
-//setTest.Dump();
+// var resultOr = await scryfallClient.Sets.AllAsync();
+// var resultOr = await scryfallClient.Sets.ByCodeAsync("uma");
+// var resultOr = await scryfallClient.Sets.ByCodeAsync("");
+// var resultOr = await scryfallClient.Sets.ByTcgPlayerIdAsync(1909);
+// var resultOr = await scryfallClient.Sets.ByIdAsync(Guid.Parse("2ec77b94-6d47-4891-a480-5d0b4e5c9372"));
+// var resultOr = await scryfallClient.Cards.SearchAsync("t:creature o:draw");
+// var resultOr = await scryfallClient.Cards.NamedExactAsync("Black Lotus");
+// var resultOr = await scryfallClient.Cards.NamedFuzzyAsync("Tarmog");
+// var resultOr = await scryfallClient.Cards.AutoCompleteAsync("Tarm");
+// var resultOr = await scryfallClient.Cards.RandomAsync();
+//var resultOr = await scryfallClient.Cards.RandomAsync("is:commander");
 
-var cardSearch = await scryfallClient.Cards.NamedExactAsync("Black Potus");
-cardSearch.Dump();
+var collectionParameters = new CollectionParametersBuilder()
+    .WithId(Guid.Parse("683a5707-cddb-494d-9b41-51b4584ded69"))
+    .WithName("Ancient Tomb")
+    .WithCollectorNumberAndSet("150", "mrd")
+    .Build();
+
+//var resultOr = await scryfallClient.Cards.CollectionAsync(collectionParameters);
+//var resultOr = await scryfallClient.Cards.ByCollectorNumberAsync("khm", "1");
+var resultOr = await scryfallClient.Cards.ByCollectorNumberAsync("khm", "1", Language.Japanese);
+
+var result = resultOr.OrElseThrow();
+result.Dump();
